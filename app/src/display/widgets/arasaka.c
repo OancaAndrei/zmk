ed0a57aa5f5a0ec279a83115c4fc3ed3e83d4932
Ionut Andrei Oanca <oancaionutandrei@gmail.com>
2024-09-08 20:55:26 +0200

    feat: yet another info animation

--- app/src/display/widgets/arasaka.c
+++ app/src/display/widgets/arasaka.c
@@ -16,6 +16,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #error "Arasaka logo widget only works with the system work queue currently"
 #endif
 
+#define ARASAKA_IDLE_GLITCH_CHANCE 90
+
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 LV_IMG_DECLARE(arasaka_1);
@@ -37,19 +39,56 @@ LV_IMG_DECLARE(arasaka_16);
 LV_IMG_DECLARE(arasaka_17);
 LV_IMG_DECLARE(arasaka_18);
 LV_IMG_DECLARE(arasaka_19);
-LV_IMG_DECLARE(arasaka_info_00);
-LV_IMG_DECLARE(arasaka_info_01);
-LV_IMG_DECLARE(arasaka_info_02);
-LV_IMG_DECLARE(arasaka_info_03);
-LV_IMG_DECLARE(arasaka_info_04);
-LV_IMG_DECLARE(arasaka_info_05);
-LV_IMG_DECLARE(arasaka_info_06);
-LV_IMG_DECLARE(arasaka_info_07);
-LV_IMG_DECLARE(arasaka_info_08);
-LV_IMG_DECLARE(arasaka_info_09);
+LV_IMG_DECLARE(arasaka_info_0);
+LV_IMG_DECLARE(arasaka_info_1);
+LV_IMG_DECLARE(arasaka_info_2);
+LV_IMG_DECLARE(arasaka_info_3);
+LV_IMG_DECLARE(arasaka_info_4);
+LV_IMG_DECLARE(arasaka_info_5);
+LV_IMG_DECLARE(arasaka_info_6);
+LV_IMG_DECLARE(arasaka_info_7);
+LV_IMG_DECLARE(arasaka_info_8);
+LV_IMG_DECLARE(arasaka_info_9);
 LV_IMG_DECLARE(arasaka_info_10);
 LV_IMG_DECLARE(arasaka_info_11);
-LV_IMG_DECLARE(arasaka_transparent);
+LV_IMG_DECLARE(arasaka_info_12);
+LV_IMG_DECLARE(arasaka_info_13);
+LV_IMG_DECLARE(arasaka_info_14);
+LV_IMG_DECLARE(arasaka_info_15);
+LV_IMG_DECLARE(arasaka_info_16);
+LV_IMG_DECLARE(arasaka_info_17);
+LV_IMG_DECLARE(arasaka_info_18);
+LV_IMG_DECLARE(arasaka_info_19);
+LV_IMG_DECLARE(arasaka_info_20);
+LV_IMG_DECLARE(arasaka_info_21);
+LV_IMG_DECLARE(arasaka_info_22);
+LV_IMG_DECLARE(arasaka_info_23);
+LV_IMG_DECLARE(arasaka_info_24);
+LV_IMG_DECLARE(arasaka_info_25);
+LV_IMG_DECLARE(arasaka_info_26);
+LV_IMG_DECLARE(arasaka_info_27);
+LV_IMG_DECLARE(arasaka_info_28);
+LV_IMG_DECLARE(arasaka_info_29);
+LV_IMG_DECLARE(arasaka_info_30);
+LV_IMG_DECLARE(arasaka_info_31);
+LV_IMG_DECLARE(arasaka_info_32);
+LV_IMG_DECLARE(arasaka_info_33);
+LV_IMG_DECLARE(arasaka_info_34);
+LV_IMG_DECLARE(arasaka_info_35);
+LV_IMG_DECLARE(arasaka_info_36);
+LV_IMG_DECLARE(arasaka_info_37);
+LV_IMG_DECLARE(arasaka_info_38);
+LV_IMG_DECLARE(arasaka_info_39);
+LV_IMG_DECLARE(arasaka_info_40);
+LV_IMG_DECLARE(arasaka_info_41);
+LV_IMG_DECLARE(arasaka_info_42);
+LV_IMG_DECLARE(arasaka_info_43);
+LV_IMG_DECLARE(arasaka_info_44);
+LV_IMG_DECLARE(arasaka_info_45);
+LV_IMG_DECLARE(arasaka_info_46);
+LV_IMG_DECLARE(arasaka_info_47);
+LV_IMG_DECLARE(arasaka_info_48);
+LV_IMG_DECLARE(arasaka_info_49);
 
 const void *images[] = {
     // arasaka_logo [0-10]
@@ -73,21 +112,57 @@ const void *images[] = {
     &arasaka_14,
     &arasaka_13,
     &arasaka_12,
-    // info [19-30]
-    &arasaka_info_11,
+    // arasaka_info [19-69]
+    &arasaka_info_0,
+    &arasaka_info_1,
+    &arasaka_info_2,
+    &arasaka_info_3,
+    &arasaka_info_4,
+    &arasaka_info_5,
+    &arasaka_info_6,
+    &arasaka_info_7,
+    &arasaka_info_8,
+    &arasaka_info_9,
     &arasaka_info_10,
-    &arasaka_info_09,
-    &arasaka_info_08,
-    &arasaka_info_07,
-    &arasaka_info_06,
-    &arasaka_info_05,
-    &arasaka_info_04,
-    &arasaka_info_03,
-    &arasaka_info_02,
-    &arasaka_info_01,
-    &arasaka_info_00,
-    // transparent [31]
-    &arasaka_transparent,
+    &arasaka_info_11,
+    &arasaka_info_12,
+    &arasaka_info_13,
+    &arasaka_info_14,
+    &arasaka_info_15,
+    &arasaka_info_16,
+    &arasaka_info_17,
+    &arasaka_info_18,
+    &arasaka_info_19,
+    &arasaka_info_20,
+    &arasaka_info_21,
+    &arasaka_info_22,
+    &arasaka_info_23,
+    &arasaka_info_24,
+    &arasaka_info_25,
+    &arasaka_info_26,
+    &arasaka_info_27,
+    &arasaka_info_28,
+    &arasaka_info_29,
+    &arasaka_info_30,
+    &arasaka_info_31,
+    &arasaka_info_32,
+    &arasaka_info_33,
+    &arasaka_info_34,
+    &arasaka_info_35,
+    &arasaka_info_36,
+    &arasaka_info_37,
+    &arasaka_info_38,
+    &arasaka_info_39,
+    &arasaka_info_40,
+    &arasaka_info_41,
+    &arasaka_info_42,
+    &arasaka_info_43,
+    &arasaka_info_44,
+    &arasaka_info_45,
+    &arasaka_info_46,
+    &arasaka_info_47,
+    &arasaka_info_48,
+    &arasaka_info_49,
 };
 
 int32_t lv_anim_path_linear_glitch(const lv_anim_t *a) {
@@ -95,7 +170,7 @@ int32_t lv_anim_path_linear_glitch(const lv_anim_t *a) {
 
     // Add the glitch effect.
     int8_t glitch_chance = lv_rand(0, 100);
-    int16_t glitch = lv_rand(-5, 5) * (glitch_chance / 70);
+    int16_t glitch = lv_rand(-5, 5) * (glitch_chance / ARASAKA_IDLE_GLITCH_CHANCE);
 
     new_value += glitch;
     if (new_value < a->start_value) new_value = a->start_value;
@@ -107,7 +182,9 @@ int32_t lv_anim_path_linear_glitch(const lv_anim_t *a) {
 int32_t lv_anim_path_idle_glitch(const lv_anim_t *a) {
     int8_t glitch_chance = lv_rand(0, 100);
 
-    if (glitch_chance < 70) return a->end_value;
+    if (glitch_chance < ARASAKA_IDLE_GLITCH_CHANCE) {
+        return a->end_value;
+    };
 
     return lv_rand(a->start_value, a->end_value);
 }
@@ -150,16 +227,8 @@ void arasaka_anim_run_qr_code_idle(struct zmk_widget_arasaka *widget) {
 }
 
 void arasaka_anim_run_info_glitch(struct zmk_widget_arasaka *widget) {
-    lv_anim_set_values(&widget->anim, 19, 30);
+    lv_anim_set_values(&widget->anim, 19, 69);
     lv_anim_set_path_cb(&widget->anim, lv_anim_path_linear_glitch);
-    lv_anim_set_time(&widget->anim, 700);
-
-    lv_anim_start(&widget->anim);
-}
-
-void arasaka_anim_run_info_idle(struct zmk_widget_arasaka *widget) {
-    lv_anim_set_values(&widget->anim, 19, 31);
-    lv_anim_set_path_cb(&widget->anim, lv_anim_path_idle_glitch);
     lv_anim_set_time(&widget->anim, 3000);
 
     lv_anim_start(&widget->anim);
@@ -168,7 +237,7 @@ void arasaka_anim_run_info_idle(struct zmk_widget_arasaka *widget) {
 void arasaka_anim_on_finish(lv_anim_t *a) {
     struct zmk_widget_arasaka *widget = (struct zmk_widget_arasaka *)lv_anim_get_user_data(a);
 
-    widget->step = (widget->step + 1) % 6;
+    widget->step = (widget->step + 1) % 5;
 
     if (widget->step == 0) {
         arasaka_anim_run_glitch(widget);
@@ -180,8 +249,6 @@ void arasaka_anim_on_finish(lv_anim_t *a) {
         arasaka_anim_run_qr_code_idle(widget);
     } else if (widget->step == 4) {
         arasaka_anim_run_info_glitch(widget);
-    } else if (widget->step == 5) {
-        arasaka_anim_run_info_idle(widget);
     }
 }
 
